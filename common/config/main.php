cce0e4dd3aa639fd7c405fde8cec4db990d862ba
Liza Pronina <proninaelizaveta2006@gmail.com>
2023-11-20 10:04:33 +0300

    module

--- common/config/main.php
+++ common/config/main.php
@@ -6,6 +6,25 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'urlManager' => [
+        'enablePrettyUrl' => true,
+        'showScriptName' => false,
+        'rules' => [ [
+            'class' => 'yii\web\GroupUrlRule',
+            'prefix' => 'da',
+            'routePrefix' => 'da',
+            'rules' => [
+                '' => 'default/index',
+                '<_m:[\w\-]+>' => '<_m>/default/index',
+                '<_m:[\w\-]+>/<id:\d+>' => '<_m>/default/view',
+                '<_m:[\w\-]+>/<id:\d+>/<_a:[\w-]+>' => '<_m>/default/<_a>',
+                '<_m:[\w\-]+>/<_c:[\w\-]+>/<id:\d+>' => '<_m>/<_c>/view',
+                '<_m:[\w\-]+>/<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => '<_m>/<_c>/<_a>',
+                '<_m:[\w\-]+>/<_c:[\w\-]+>' => '<_m>/<_c>/index',
+            ],
+        ],
+        ],
+    ],
         'cache' => [
             'class' => \yii\caching\FileCache::class,
         ],
