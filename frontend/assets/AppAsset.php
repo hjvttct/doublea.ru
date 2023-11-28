0810c708fd3f603eb3705028e9d38789b48f25e3
Roman Lyustochkin <roman135785@gmail.com>
2023-11-28 09:31:44 +0300

    module pd

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,7 +13,8 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
-        'css/style.css',
+        'css/style1.css',
+
     ];
     public $js = [
     ];
